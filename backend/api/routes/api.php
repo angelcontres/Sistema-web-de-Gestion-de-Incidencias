6143a48c9d922b36ac3d446543b830890c836d91
CarlosFernandoPatinoGarcia <carlos.patinogarcia9447@upse.edu.ec>
2026-06-02 00:08:04 -0500

    feat(OpcionMenu) implementar endpoint conectado al controlador
    - crear el controller usando Route::apiResource, que ya encapsula todas las funciones y las ejecuta según las que estén implementadas en el controlador
    
    - implementar tests con casos de prueba en la gestión del registro

--- backend/api/routes/api.php
+++ backend/api/routes/api.php
@@ -1,7 +1,26 @@
 <?php
 
+use App\Http\Controllers\OpcionMenuController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+// Solo crea las rutas para index y show
+// Route::apiResource('/opciones-menu', OpcionMenuController::class)->only(['index', 'show']);
+
+
+/**
+ * Ruta base para opciones de menú
+ * Con apiResourse se mapean todas las funciones REST, por lo cual no se requiere definir cada ruta individualmente.
+ * GET     /opciones-menu           index
+ * POST    /opciones-menu           store
+ * GET     /opciones-menu/{id}      show
+ * PUT     /opciones-menu/{id}      update
+ * DELETE  /opciones-menu/{id}      destroy
+ * La respectiva lógica se encontrará en el controlador OpcionMenuController
+ */
+Route::apiResource('/opciones-menu', OpcionMenuController::class);
+// Route::post('/opciones-menu', OpcionMenuController::class->store('',''));
+
 
 // Endpoint de prueba sin base de datos
 Route::get('/productos', function () {
