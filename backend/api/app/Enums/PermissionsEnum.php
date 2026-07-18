fdc84ed745c4bd46c85116f8d95947ac8caacf9e
CarlosFernandoPatinoGarcia <carlos.patinogarcia9447@upse.edu.ec>
2026-07-18 15:33:29 -0500

    feat(permissions): habilitar registro manual de recurso en la creacion de permisos
    - Se agrega el campo 'recurso' al modal de creacion/edicion de permisos en el frontend.
    - Se hace opcional el campo 'opcion_menu_id' en la UI.
    - Se ajusta PermisoController en el backend para almacenar el recurso ingresado desde el request en lugar de adivinarlo estrictamente de la ruta del menu.
    - se agrega recurso mantenimiento para controlar acceso a ese menu

--- backend/api/app/Enums/PermissionsEnum.php
+++ backend/api/app/Enums/PermissionsEnum.php
@@ -93,4 +93,10 @@ enum PermissionsEnum: string
     case CREATE_HISTORIAL = 'Crear Historial';
     case UPDATE_HISTORIAL = 'Actualizar Historial';
     case DELETE_HISTORIAL = 'Eliminar Historial';
+
+    // Mantenimiento
+    case READ_MANTENIMIENTO = 'Ver Mantenimiento';
+    case CREATE_MANTENIMIENTO = 'Crear Mantenimiento';
+    case UPDATE_MANTENIMIENTO = 'Actualizar Mantenimiento';
+    case DELETE_MANTENIMIENTO = 'Eliminar Mantenimiento';
 }
