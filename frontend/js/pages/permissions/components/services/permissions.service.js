394e5d02009a1d89b5338ea73a59566cdb7364a6
CarlosFernandoPatinoGarcia <carlos.patinogarcia9447@upse.edu.ec>
2026-07-19 23:31:06 -0500

    Update permissions.service.js

--- frontend/js/pages/permissions/components/services/permissions.service.js
+++ frontend/js/pages/permissions/components/services/permissions.service.js
@@ -1,10 +1,12 @@
-import { apiRequest } from '../../../../../core/api.js';
+import { apiRequest } from '../../../../core/api';
 
 export const PermissionService = {
-  getAll: (page = 1, filters = '') => apiRequest(`/permissions?page=${page}${filters ? '&' + filters : ''}`),
+  getAll: (page = 1, filters = '') =>
+    apiRequest(`/permissions?page=${page}${filters ? '&' + filters : ''}`),
   getAllList: () => apiRequest('/permissions?all=true'),
   getById: (id) => apiRequest(`/permissions/${id}`),
   create: (data) => apiRequest('/permissions', { method: 'POST', body: JSON.stringify(data) }),
-  update: (id, data) => apiRequest(`/permissions/${id}`, { method: 'PUT', body: JSON.stringify(data) }),
+  update: (id, data) =>
+    apiRequest(`/permissions/${id}`, { method: 'PUT', body: JSON.stringify(data) }),
   delete: (id) => apiRequest(`/permissions/${id}`, { method: 'DELETE' }),
 };
